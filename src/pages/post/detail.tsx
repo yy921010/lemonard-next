35ff11898edf4e8d748aea8c5cf5c80a4bc58d29
charles <charlesmaxwellyoung@gmail.com>
2021-01-20 22:31:13 +0800

    refactor: 优化代码结构

--- src/pages/post/detail.tsx
+++ src/pages/post/detail.tsx
@@ -1,12 +1,10 @@
 import Head from 'next/head'
-import Layout from '../../components/layout'
 export default function Detail() {
   return (
     <>
       <Head>
         <title>detail page</title>
       </Head>
-      <Layout>detail page </Layout>
     </>
   )
 }
