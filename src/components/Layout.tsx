e151a0fabaeda282a49cd1c01ced000e62b91034
charles <charlesmaxwellyoung@gmail.com>
2021-01-21 23:50:51 +0800

    feat: 全局css 调整

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -19,30 +19,31 @@ export const Layout = ({ children }) => {
     },
   ]
   return (
-    <div className="flex flex-column absolute top-20 left-0 right-0 justify-center h-full bg-gray-700">
-      <div className="flex top-0 left-0 right-0 fixed bg-gray-800 justify-center">
-        <header className="flex items-center justify-between container h-20">
-          <div className="w-16 h-16"></div>
-          <ul className="flex flex-shrink">
+    <>
+      <nav className="fixed top-0 left-0 w-full bg-gray-800 justify-center z-10">
+        <div className="flex items-center justify-between container h-20 mx-auto">
+          <div className="w-16 h-16 bg-red-400"></div>
+          <ul className="flex flex-shrink space-x-8">
             {routes
               .filter((item) => item.isShown)
               .map((item) => {
                 return (
-                  <li key={item.name} className="cursor-pointer text-xl mr-9">
+                  <li key={item.name} className="cursor-pointer text-xl">
                     <Link href={item.path}>{item.name}</Link>
                   </li>
                 )
               })}
           </ul>
-          <div className="flex">
-            <div className="cursor-pointer mr-3.5">sign in</div>
-            <div className="cursor-pointer mr-3.5">sign up</div>
+          <div className="flex space-x-2">
+            <div className="cursor-pointer">sign in</div>
+            <div className="cursor-pointer">sign up</div>
           </div>
-        </header>
-      </div>
-      <section className="flex flex-col items-center">
-        <section className="flex-1">{children}</section>
+        </div>
+      </nav>
+      <section className="flex flex-col items-center overflow-y-auto">
+        <section className="w-full flex-shrink">{children}</section>
+        <footer className="container border-gray-900 w-full h-72 border-solid border-t-2"></footer>
       </section>
-    </div>
+    </>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,16 +8,18 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout>
-        <footer>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Powered by{' '}
-            <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-          </a>
-        </footer>
+        <div className="container mx-auto">
+          <footer>
+            <a
+              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Powered by{' '}
+              <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
+            </a>
+          </footer>
+        </div>
       </Layout>
     </>
   )
